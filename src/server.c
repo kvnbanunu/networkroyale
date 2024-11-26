73b84cd42e6fc7c2cc07eef21c40dcf5d08f6bb5
kvnbanunu <kvnbanunu@gmail.com>
2024-11-26 13:05:40 -0800

    Uses threads to handle each player connection.

--- src/server.c
+++ src/server.c
@@ -3,6 +3,7 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <poll.h>
+#include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,37 +27,47 @@ typedef struct Player
     char               username[NAME_LEN];
 } player_t;
 
-void        handle_new_player(int serverfd, struct pollfd *fds, nfds_t *nfds, player_t *player, int *player_count, in_port_t udp_port);
+typedef struct ThreadData
+{
+    int           serverfd;
+    int           player_count;
+    player_t      players[MAX_PLAYERS];
+    struct pollfd fds[MAX_PLAYERS + 2];
+    nfds_t        nfds;
+    in_port_t     udp_port;
+} thread_data_t;
+
+void       *handle_new_player(void *arg);
+void        new_player(int serverfd, struct pollfd *fds, nfds_t *nfds, player_t *player, int *player_count, in_port_t udp_port);
 static void setup_signal_handler(void);
 static void sig_handler(int sig);
 
 int main(void)
 {
-    int                tcpfd;
     int                udpfd;
-    int                retval       = EXIT_FAILURE;
-    int                player_count = 0;
+    int                retval = EXIT_FAILURE;
     char               address_str[INET_ADDRSTRLEN];
     const char        *game_start_message = "Game starting...\n";
     struct sockaddr_in tcp_addr;
     struct sockaddr_in udp_addr;
-    struct pollfd      fds[MAX_PLAYERS + 2];    // Max players + server + keyboard
-    nfds_t             nfds;
-    player_t           players[MAX_PLAYERS];
     in_addr_t          address;
     in_port_t          tcp_port;
-    in_port_t          udp_port;
     socklen_t          addr_len = sizeof(struct sockaddr);
+    thread_data_t      thread_data;
+    pthread_t          thread;
 
     setup_signal_handler();
 
     // Get the first IPv4 address of this system that matches 192.168.*.*
     findaddress(&address, address_str);
 
-    tcp_port = setup_and_bind(&tcpfd, &tcp_addr, address, addr_len, SOCK_STREAM);
-    udp_port = setup_and_bind(&udpfd, &udp_addr, address, addr_len, SOCK_DGRAM);
+    memset(&thread_data, 0, sizeof(thread_data));
+    thread_data.player_count = 0;
 
-    if(listen(tcpfd, MAX_PLAYERS) == -1)
+    tcp_port             = setup_and_bind(&(thread_data.serverfd), &tcp_addr, address, addr_len, SOCK_STREAM);
+    thread_data.udp_port = setup_and_bind(&udpfd, &udp_addr, address, addr_len, SOCK_DGRAM);
+
+    if(listen(thread_data.serverfd, MAX_PLAYERS) == -1)
     {
         perror("listen");
         goto done;
@@ -65,35 +76,42 @@ int main(void)
     printf("Server waiting for players on %s : %d\nPress Enter to start game...\n", address_str, ntohs(tcp_port));
 
     // Setup to poll the server fd and keyboard input
-    memset(fds, 0, sizeof(fds));
-    fds[0].fd     = tcpfd;
-    fds[0].events = POLLIN;
-    fds[1].fd     = STDIN_FILENO;
-    fds[1].events = POLLIN;
-    nfds          = 2;
-    memset(players, 0, sizeof(players));
+    thread_data.fds[0].fd     = thread_data.serverfd;
+    thread_data.fds[0].events = POLLIN;
+    thread_data.fds[1].fd     = STDIN_FILENO;
+    thread_data.fds[1].events = POLLIN;
+    thread_data.nfds          = 2;
     while(!exit_flag)
     {
         // poll because we want to check for non-blocking keyboard input
-        if(poll(fds, nfds, -1) < 0)
+        if(poll(thread_data.fds, thread_data.nfds, -1) < 0)
         {
             perror("poll");
             goto done;
         }
 
-        for(nfds_t i = 0; i < nfds; i++)
+        for(nfds_t i = 0; i < thread_data.nfds; i++)
         {
-            if(fds[i].revents & POLLIN)    // funky way to check
+            if(thread_data.fds[i].revents & POLLIN)    // funky way to check
             {
-                if(fds[i].fd == tcpfd)
+                if(thread_data.fds[i].fd == thread_data.serverfd)
                 {
-                    handle_new_player(tcpfd, fds, &nfds, &players[player_count], &player_count, udp_port);
-                    if(player_count >= MAX_PLAYERS)
+                    // TODO change to thread.
+                    // handle_new_player(thread_data.serverfd, thread_data.fds, &(thread_data.nfds), &(thread_data.players[thread_data.player_count]), &(thread_data.player_count), thread_data.udp_port);
+                    if(pthread_create(&thread, NULL, handle_new_player, (void *)&thread_data) != 0)
+                    {
+                        perror("pthread_create");
+                        goto done;
+                    }
+
+                    pthread_join(thread, NULL);
+
+                    if(thread_data.player_count >= MAX_PLAYERS)
                     {
                         goto game_start;
                     }
                 }
-                else if(fds[i].fd == STDIN_FILENO)
+                else if(thread_data.fds[i].fd == STDIN_FILENO)
                 {
                     char input;
                     if(read(STDIN_FILENO, &input, 1) < 1)
@@ -112,20 +130,69 @@ int main(void)
     }
 game_start:
     // Starting game aka closing connections
-    for(int i = 0; i < player_count; i++)
+    for(int i = 0; i < thread_data.player_count; i++)
     {
-        write(players[i].socket, game_start_message, GAME_START_LEN);
-        close(players[i].socket);
+        write(thread_data.players[i].socket, game_start_message, GAME_START_LEN);
+        close(thread_data.players[i].socket);
     }
     retval = EXIT_SUCCESS;
 
 done:
-    socket_close(tcpfd);
+    socket_close(thread_data.serverfd);
     socket_close(udpfd);
     return retval;
 }
 
-void handle_new_player(int serverfd, struct pollfd *fds, nfds_t *nfds, player_t *player, int *player_count, in_port_t udp_port)
+void *handle_new_player(void *arg)
+{
+    thread_data_t     *data = (thread_data_t *)arg;
+    struct sockaddr_in player_addr;
+    socklen_t          addr_len;
+    int                playerfd;
+    uint8_t            buf[INFO_LEN];
+    in_port_t          playerport;
+    char               client_host[NI_MAXHOST];
+    player_t          *player = &(data->players[data->player_count]);
+
+    if(data->player_count >= MAX_PLAYERS)
+    {
+        printf("Maximum player count reached.\n");
+        pthread_exit(NULL);
+    }
+
+    addr_len = sizeof(player_addr);
+    playerfd = accept(data->serverfd, (struct sockaddr *)&player_addr, &addr_len);
+    if(playerfd == -1)
+    {
+        perror("accept");
+        pthread_exit(NULL);
+    }
+    read(playerfd, buf, INFO_LEN);
+    memcpy(player->username, buf, NAME_LEN);
+    memcpy(&playerport, &buf[NAME_LEN], sizeof(in_port_t));
+    player->id     = data->player_count;
+    player->socket = playerfd;
+
+    getnameinfo((struct sockaddr *)&player_addr, addr_len, client_host, NI_MAXHOST, NULL, 0, 0);
+
+    player->addr          = player_addr;
+    player->addr.sin_port = playerport;
+
+    data->player_count++;
+
+    // add player socket to poll
+    data->fds[data->nfds].fd     = playerfd;
+    data->fds[data->nfds].events = POLLIN;
+    data->nfds++;
+
+    // send udp port
+    write(playerfd, &data->udp_port, sizeof(in_port_t));
+
+    printf("Player %s joined the lobby from %s:%d\n", player->username, client_host, ntohs(player->addr.sin_port));
+    return NULL;
+}
+
+void new_player(int serverfd, struct pollfd *fds, nfds_t *nfds, player_t *player, int *player_count, in_port_t udp_port)
 {
     struct sockaddr_in player_addr;
     socklen_t          addr_len;
